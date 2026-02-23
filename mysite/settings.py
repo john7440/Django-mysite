d862f643f2bfe975580b05f2b791259d1696c4d5
Jonathan M. <neodev7440@gmail.com>
2026-02-23 11:27:38 +0100

    modification des settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,9 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
+USE_I18N = True
 
 USE_I18N = True
 
