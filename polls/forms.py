d78fd773711d656feb0e8eb5ce374314fb2e0b2b
Jonathan M. <neodev7440@gmail.com>
2026-02-26 14:46:48 +0100

    amélioration de la lisibilité dans le QuestionForm

--- polls/forms.py
+++ polls/forms.py
@@ -8,13 +8,13 @@ la classe Meta sert a config le formulaire:
 - lasbels : personnalisation du texte
 """
 class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question_text']
+        labels = {'question_text': 'Question'}
+
     choice_1 = forms.CharField(max_length=200, required=False, label='Choix 1')
     choice_2 = forms.CharField(max_length=200, required=False, label='Choix 2')
     choice_3 = forms.CharField(max_length=200, required=False, label='Choix 3')
     choice_4 = forms.CharField(max_length=200, required=False, label='Choix 4')
     choice_5 = forms.CharField(max_length=200, required=False, label='Choix 5')
-
-    class Meta:
-        model = Question
-        fields = ['question_text']
-        labels = {'question_text': 'Question'}
