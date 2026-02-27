157b19416498b79325783c8ce64acda58dd61947
Jonathan M. <neodev7440@gmail.com>
2026-02-27 12:45:22 +0100

    ajout des setting + url pour le login ainsi que le login.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+LOGIN_URL = '/accounts/login/'
+
+LOGIN_REDIRECT_URL = '/polls/'
+
+LOGOUT_REDIRECT_URL = '/polls/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ] + debug_toolbar_urls()
