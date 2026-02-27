a4bb676e30b276b617141e71b532df0bf2be7d51
Jonathan M. <neodev7440@gmail.com>
2026-02-27 14:15:21 +0100

    ajout des widgets pour avoir le formulaire avec le style de bootstrap

--- polls/forms.py
+++ polls/forms.py
@@ -12,9 +12,17 @@ class QuestionForm(forms.ModelForm):
         model = Question
         fields = ['question_text']
         labels = {'question_text': 'Question'}
+        widgets = {
+            'question_text': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
-    choice_1 = forms.CharField(max_length=200, required=False, label='Choix 1')
-    choice_2 = forms.CharField(max_length=200, required=False, label='Choix 2')
-    choice_3 = forms.CharField(max_length=200, required=False, label='Choix 3')
-    choice_4 = forms.CharField(max_length=200, required=False, label='Choix 4')
-    choice_5 = forms.CharField(max_length=200, required=False, label='Choix 5')
+    choice_1 = forms.CharField(max_length=200, required=False, label='Choix 1',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    choice_2 = forms.CharField(max_length=200, required=False, label='Choix 2',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    choice_3 = forms.CharField(max_length=200, required=False, label='Choix 3',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    choice_4 = forms.CharField(max_length=200, required=False, label='Choix 4',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    choice_5 = forms.CharField(max_length=200, required=False, label='Choix 5',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
