baec7f09b95cd111c97acdf87f4ef44d9c79e5db
Jonathan M. <neodev7440@gmail.com>
2026-03-02 15:09:37 +0100

    ajout de tests pour les vote + amélioration test creation avec 3 choix

--- polls/tests.py
+++ polls/tests.py
@@ -5,17 +5,21 @@ from django.urls import reverse
 from django.utils import timezone
 from django.test import TestCase, Client
 
-from .models import Question
+from .models import Question, Choice
+
 
 #-------------tests questions--------------------------
-def create_question(question_text,days):
+def create_question(text,days = 0, nb_choices = 0):
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    q = Question.objects.create(question_text=text, pub_date=time)
+    for i in range(1, nb_choices +1):
+        Choice.objects.create(question=q, choice_text=f"Choix {i}", votes=i)
+    return q
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
@@ -32,7 +36,7 @@ class QuestionIndexViewTests(TestCase):
         Questions with a pub_date in the past are displayed on the
         index page.
         """
-        question = create_question(question_text='Past question.', days=-30)
+        question = create_question(text='Past question.', days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerySetEqual(response.context['latest_question_list'], [question])
 
@@ -41,7 +45,7 @@ class QuestionIndexViewTests(TestCase):
         Questions with a pub_date in the future aren't displayed on
         the index page.
         """
-        create_question(question_text='Future question.', days=30)
+        create_question(text='Future question.', days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, 'Aucun sondage disponible')
         self.assertQuerySetEqual(response.context['latest_question_list'], [])
@@ -51,8 +55,8 @@ class QuestionIndexViewTests(TestCase):
         Even if both past and future questions exist, only past questions
         are displayed.
         """
-        question = create_question(question_text='Past question.', days=-30)
-        create_question(question_text='Future question.', days=30)
+        question = create_question(text='Past question.', days=-30)
+        create_question(text='Future question.', days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerySetEqual(response.context['latest_question_list'], [question])
 
@@ -60,8 +64,8 @@ class QuestionIndexViewTests(TestCase):
         """
         The questions index page may display multiple questions.
         """
-        question1 = create_question(question_text='Past question 1.', days=-30)
-        question2 = create_question(question_text='Past question 2.', days=-5)
+        question1 = create_question(text='Past question 1.', days=-30)
+        question2 = create_question(text='Past question 2.', days=-5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerySetEqual(response.context['latest_question_list'], [question2, question1])
 
@@ -72,7 +76,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
-        future_question = create_question(question_text='Future question.', days=5)
+        future_question = create_question(text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -82,7 +86,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text='Past question.', days=-5)
+        past_question = create_question(text='Past question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
@@ -104,23 +108,18 @@ class AddQuestionViewTest(TestCase):
         self.assertEqual(Question.objects.count(), 1)
         self.assertEqual(Question.objects.first().question_text, 'Question de test ?')
 
-    #test cration avec 3 choix
+    #test creation avec 3 choix
     def test_post_question_with_choices(self):
-        response = self.client.post(self.url, {
-            'question_text': 'Couleur préférée ?',
+        self.client.post(self.url, {
+            'question_text': 'Avec choix ?',
             'choice_1': 'Rouge',
             'choice_2': 'Bleu',
             'choice_3': 'Vert',
             'choice_4': '',
             'choice_5': '',
         })
-
-        question = Question.objects.first()
-        self.assertEqual(question.choice_set.count(), 3)
-        textes = list(question.choice_set.values_list('choice_text', flat=True))
-        self.assertIn('Rouge', textes)
-        self.assertIn('Bleu', textes)
-        self.assertIn('Vert', textes)
+        q = Question.objects.first()
+        self.assertEqual(q.choice_set.count(), 3)
 
 #----------------Test Questions-------------------------
 class QuestionModelTests(TestCase):
@@ -168,3 +167,22 @@ class AuthTests(TestCase):
         self.client.login(username='testuser', password='testpassword')
         response = self.client.post(reverse('logout'))
         self.assertRedirects(response,'/polls/')
+
+#----------------------tests votes--------------------
+class VoteViewTests(TestCase):
+   def test_vote_increments_choice(self):
+       q = create_question("Vote ?", nb_choices=2)
+       choice = q.choice_set.first()
+       past_votes = choice.votes
+       self.client.post(reverse('polls:vote', args=(q.id,)),{'choice': choice.id})
+       choice.refresh_from_db()
+       self.assertEqual(choice.votes, past_votes+1)
+
+   def test_vote_redirects_to_frequency(self):
+       q = create_question("Redirect ?", nb_choices=1)
+       choice = q.choice_set.first()
+       response = self.client.post(
+           reverse('polls:vote', args=(q.id,)),
+           {'choice': choice.id}
+       )
+       self.assertRedirects(response, reverse('polls:frequency', args=(q.id,)))
