5bcc4185c175aac73d1647609913c8b6c1efc63c
Jonathan M. <neodev7440@gmail.com>
2026-03-02 08:46:46 +0100

    ajout d'une route par defaut

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("polls.urls")),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
